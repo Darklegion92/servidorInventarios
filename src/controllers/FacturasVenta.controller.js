dfa0294f77ce30c5fcaf4e98e12874ec586f7238
Desarrollo <onlin1992@gmail.com>
2020-07-29 22:22:17 -0500

    Produccion agregados impresiones y modificaciones de impresiones para que salgan 3 decimales

--- src/controllers/FacturasVenta.controller.js
+++ src/controllers/FacturasVenta.controller.js
@@ -26,7 +26,7 @@ async function crear(req, res) {
             articulo.codigoarticulo,
             articulo.descripcionarticulo,
             articulo.cantidadarticulo,
-            parseFloat(articulo.valorarticulo),
+            parseFloat(articulo.valorarticulo.replace(".","")),
             parseFloat(articulo.ivaarticulo) / articulo.cantidadarticulo,
             articulo.idtarifaiva,
             idfactura_venta,
@@ -42,6 +42,73 @@ async function crear(req, res) {
     } else res.status(201).send({ mensaje: "No Se Actualizo El Campo" });
   } catch (e) {
     res.status(501).send({ mensaje: "Error " + e });
+  }
+}
+
+async function devoluciones(req, res) {
+  res.setHeader("Content-Type", "application/json");
+  const { articulos } = req.body;
+  const { idusuario } = req;
+  const fechacreacion = new Date();
+  let datos;
+  try {
+    await articulos.forEach(async (articulo) => {
+      const idfactura_venta_detalle = articulo;
+      datos = await pool.query("INSERT INTO devoluciones_ventas SET ?", {
+        idfactura_venta_detalle,
+        idusuario,
+        fechacreacion,
+      });
+    });
+    res.status(200).send({
+      mensaje: "Se han devuelto los artículos",
+    });
+  } catch (e) {
+    res.status(501).send({ mensaje: "Error " + e });
+  }
+}
+
+async function consultarNumero(req, res) {
+  res.setHeader("Content-Type", "application/json");
+  const { numero } = req.params;
+
+  try {
+    const datos = await pool.query(
+      "SELECT f.*, concat(c.nombres,' ',c.apellidos) AS nombre, c.documento FROM facturas_venta f, clientes c " +
+        "WHERE c.idcliente = f.idcliente AND iddevolucion_ventas is null AND  ?",
+      {
+        numero,
+      }
+    );
+
+    if (datos[0]) {
+      const articulos = await pool.query(
+        "SELECT *, cantidadarticulo * valorarticulo as total FROM factura_venta_detalles d WHERE idfactura_venta = ?",
+        datos[0].idfactura_venta
+      );
+      let data = datos[0];
+      data.articulos = articulos;
+      res.status(200).send(data);
+    } else res.status(201).send({ mensaje: "No Se Encontraron Valores" });
+  } catch (e) {
+    res.status(501).send({ mensaje: "Error " + e });
+    console.log(e);
+  }
+}
+
+async function ventasDia(req, res) {
+  res.setHeader("Content-Type", "application/json");
+
+  try {
+    const datos = await pool.query(
+      "SELECT SUM(d.cantidadarticulo*d.valorarticulo) as total FROM factura_venta_detalles d, facturas_venta f "+
+      "WHERE f.idfactura_venta = d.idfactura_venta AND f.fechacreacion>=CURDATE()");
+    console.log(datos)
+    if (datos[0]) {
+      res.status(200).send(datos);
+    } else res.status(201).send({ mensaje: "No Se Encontraron Valores" });
+  } catch (e) {
+    res.status(501).send({ mensaje: "Error " + e });
     console.log(e);
   }
 }
@@ -53,5 +120,8 @@ function error(req, res) {
 
 module.exports = {
   crear,
+  consultarNumero,
+  devoluciones,
+  ventasDia,
   error,
 };
--- src/routes/FacturasVenta.routes.js
+++ src/routes/FacturasVenta.routes.js
@@ -3,6 +3,11 @@ const Auth = require("../middlewares/acceso");
 const FacturasCtrl = require("../controllers/FacturasVenta.controller");
 
 router = Router();
-router.post("/", Auth.isAuth, FacturasCtrl.crear).get("/*", FacturasCtrl.error);
+router
+  .post("/", Auth.isAuth, FacturasCtrl.crear)
+  .post("/devolucion", Auth.isAuth, FacturasCtrl.devoluciones)
+  .get("/ventasdia", FacturasCtrl.ventasDia)
+  .get("/:numero", FacturasCtrl.consultarNumero)
+  .get("/*", FacturasCtrl.error);
 
 module.exports = router;
